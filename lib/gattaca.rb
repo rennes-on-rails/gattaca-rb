ee527b94a533f71c01c3ad6996536caa36822938
thierry <thierry.henrio@gmail.com>
2011-12-27 17:49:21 +0100

    oops, back to green and prepare code for next example map is not enough

--- lib/gattaca.rb
+++ lib/gattaca.rb
@@ -1,6 +1,19 @@
 module Gattaca
   def analyse(predictions)
-    predictions.map { |p| [p] }
+    head, *tail = predictions
+    tail.reduce([ [head] ]) do |sequences, p|
+      sequences.reduce( [] ) do |acc, sequence|
+        acc.push(*mutate(sequence, p))
+      end
+    end
   end
-  module_function :analyse
+
+  def mutate(sequence, p)
+    if sequence.last[1] < p[0]
+    else
+      [sequence, [p]]
+    end
+  end
+
+  module_function :analyse, :mutate
 end

447634d590739fded91cd16744741c532635b094
thierry <thierry.henrio@gmail.com>
2011-12-27 18:36:23 +0100

    "optimized" analyse : produces less sequences

--- lib/gattaca.rb
+++ lib/gattaca.rb
@@ -3,17 +3,22 @@ module Gattaca
   def analyse(predictions)
     head, *tail = predictions.sort
     tail.reduce([ [head] ]) do |sequences, p|
-      sequences.reduce( [] ) do |acc, sequence|
-        acc.push(*mutate(sequence, p))
+      count, sequences = sequences.reduce( [0, []] ) do |acc, sequence|
+        count, list = acc
+        inc, ng = mutate(sequence, p)
+        [count + inc, list.push(*ng)]
       end
+      sequences << [p] if count.zero?
+      sequences
     end
   end
 
   def mutate(sequence, p)
     sub = sequence.select { |pp| pp[1] < p[0] }
     case sub.count
-      when sequence.count then [sequence << p]
-      else [sequence, sub << p]
+      when sequence.count then [1, [sequence << p]]
+      when 0 then [0, [sequence]]
+      else [1, [sequence, sub << p]]
     end
   end
 

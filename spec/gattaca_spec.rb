0f3f493a221bcf0236e8605ff544efcc4503e62d
thierry <thierry.henrio@gmail.com>
2011-12-27 17:41:00 +0100

    fake first example

--- spec/gattaca_spec.rb
+++ spec/gattaca_spec.rb
@@ -1,8 +1,10 @@
 require 'minitest/autorun'
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
+require 'gattaca'
 
 describe Gattaca do
   describe ".analyse" do
-    it "returns [[[1, 10, 10]]] for [ [1, 10, 10] ]" do
+    it "can make a single sequence with [ [1, 10, 10] ]" do
       Gattaca.analyse([ [1, 10, 10] ]).must_equal [ [[1, 10, 10]] ]
     end
   end

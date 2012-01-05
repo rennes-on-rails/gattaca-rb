4a4aeebf84cab4d774f43d17b90e4112afa1d7b6
thierry <thierry.henrio@gmail.com>
2012-01-05 19:03:16 +0100

    can separate overlapping predictions

--- spec/gattaca_spec.rb
+++ spec/gattaca_spec.rb
@@ -8,5 +8,8 @@ describe Gattaca do
     it "returns [[[1, 10, 10]]] for [ [1, 10, 10] ]" do
       Gattaca.analyse([ [1, 10, 10] ]).must_equal [ [[1, 10, 10]] ]
     end
+    it "can separate overlapping predictions [1, 10, 10], [5, 15, 10]" do
+      Gattaca.analyse([ [1, 10, 10], [5, 15, 10] ]).must_equal [ [[1, 10, 10]], [[5, 15, 10]] ]
+    end
   end
 end
